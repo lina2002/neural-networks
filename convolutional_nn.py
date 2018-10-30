8e835b4a07171bf9ff59c659cf6eff277d5d821a
Agnieszka Paszek <apaszek@mezzobit.com>
2018-10-30 03:19:45 +0100

    in progress 2

--- convolutional_nn.py
+++ convolutional_nn.py
@@ -22,10 +22,11 @@ class ConvolutionalNN:
 
     def _feed(self, X, weights):
         c = convolve(X, weights[0], mode='valid', axes=([1, 2], [1, 2]))
-        m = np.zeros([c.shape[0], c.shape[1], c.shape[2]//2, c.shape[3]//2])
+        m = np.zeros((c.shape[0], c.shape[1], c.shape[2]//2, c.shape[3]//2))
+        print(c.shape)
         for i in range(c.shape[0]):
             for j in range(c.shape[1]):
-                m[i][j] = maxout(c[i][j])
+                copy(maxout(c[i, j]), m[i, j])
         m = np.reshape(m, (self.batch_size, -1))
         z = np.dot(m, weights[1])
         return softmax(z)
@@ -38,9 +39,8 @@ class ConvolutionalNN:
         m = np.zeros([c.shape[0], c.shape[1], c.shape[2]//2, c.shape[3]//2])
         for i in range(c.shape[0]):
             for j in range(c.shape[1]):
-                print(m[i, j, :, :].shape)
-                m[i, j, :, :] = maxout(c[i][j])
-        m = np.reshape(m, (self.batch_size, -1))
+                copy(maxout(c[i, j]), m[i, j])
+        m = np.reshape(m, (self.X.shape[0], -1))
         z = np.dot(m, weights[1])
         return -np.sum(np.sum((z - logsumexp(z, axis=1, keepdims=True))*y, axis=1))/X.shape[0]
 
@@ -54,14 +54,14 @@ class ConvolutionalNN:
             for i in range(0, X.shape[0], self.batch_size):
                 selected_data_points = np.take(permuted_indices, range(i, i+self.batch_size), mode='wrap')
                 delta_w = self._d_cost(X[selected_data_points], y[selected_data_points], self.weights)
-                for w, d in zip(self.weights, delta_w):
-                    w -= d*self.learning_rate
-
-            training_accuracy = compute_accuracy(self.predict(X, self.weights), np.argmax(y, 1))
-            validation_accuracy = compute_accuracy(self.predict(X_valid, self.weights), np.argmax(y_valid, 1))
-
-            print("training accuracy: " + str(round(training_accuracy, 2)))
-            print("validation accuracy: " + str(round(validation_accuracy, 2)))
+            #     for w, d in zip(self.weights, delta_w):
+            #         w -= d*self.learning_rate
+            #
+            # training_accuracy = compute_accuracy(self.predict(X, self.weights), np.argmax(y, 1))
+            # validation_accuracy = compute_accuracy(self.predict(X_valid, self.weights), np.argmax(y_valid, 1))
+            #
+            # print("training accuracy: " + str(round(training_accuracy, 2)))
+            # print("validation accuracy: " + str(round(validation_accuracy, 2)))
 
             print("cost: " + str(self._cost(X, y, self.weights)))
 
@@ -69,3 +69,9 @@ class ConvolutionalNN:
 def maxout(a):
     return a[:13, :13]
     # return skimage.measure.block_reduce(a, (2,2), np.max)
+
+
+def copy(a_from, a_to):
+    for i in range(a_from.shape[0]):
+        for j in range(a_from.shape[1]):
+            a_to[i, j] = a_from[i, j]
