61d8ff6b5c2938b2eb7359f7434724f42d41b293
Agnieszka Paszek <apaszek@mezzobit.com>
2018-10-30 02:27:52 +0100

    in progress

--- convolutional_nn.py
+++ convolutional_nn.py
@@ -2,6 +2,9 @@ import skimage
 from autograd import numpy as np, grad
 from autograd.scipy.misc import logsumexp
 from autograd.scipy.signal import convolve
+from sklearn.utils.extmath import softmax
+
+from utils import compute_accuracy
 
 
 class ConvolutionalNN:
@@ -17,12 +20,26 @@ class ConvolutionalNN:
         self.weights.append(2*init_scale*np.random.rand(N, 3, 3) - init_scale)
         self.weights.append(2*init_scale*np.random.rand(8*13*13, 10) - init_scale)
 
-    def _cost(self, X, y, weights):
+    def _feed(self, X, weights):
         c = convolve(X, weights[0], mode='valid', axes=([1, 2], [1, 2]))
         m = np.zeros([c.shape[0], c.shape[1], c.shape[2]//2, c.shape[3]//2])
         for i in range(c.shape[0]):
             for j in range(c.shape[1]):
                 m[i][j] = maxout(c[i][j])
+        m = np.reshape(m, (self.batch_size, -1))
+        z = np.dot(m, weights[1])
+        return softmax(z)
+
+    def predict(self, X):
+        return np.argmax(self._feed(X, self.weights), 1)
+
+    def _cost(self, X, y, weights):
+        c = convolve(X, weights[0], mode='valid', axes=([1, 2], [1, 2]))
+        m = np.zeros([c.shape[0], c.shape[1], c.shape[2]//2, c.shape[3]//2])
+        for i in range(c.shape[0]):
+            for j in range(c.shape[1]):
+                print(m[i, j, :, :].shape)
+                m[i, j, :, :] = maxout(c[i][j])
         m = np.reshape(m, (self.batch_size, -1))
         z = np.dot(m, weights[1])
         return -np.sum(np.sum((z - logsumexp(z, axis=1, keepdims=True))*y, axis=1))/X.shape[0]
@@ -36,8 +53,19 @@ class ConvolutionalNN:
             permuted_indices = np.random.permutation(X.shape[0])
             for i in range(0, X.shape[0], self.batch_size):
                 selected_data_points = np.take(permuted_indices, range(i, i+self.batch_size), mode='wrap')
-                self._cost(X[selected_data_points], y[selected_data_points], self.weights)
+                delta_w = self._d_cost(X[selected_data_points], y[selected_data_points], self.weights)
+                for w, d in zip(self.weights, delta_w):
+                    w -= d*self.learning_rate
+
+            training_accuracy = compute_accuracy(self.predict(X, self.weights), np.argmax(y, 1))
+            validation_accuracy = compute_accuracy(self.predict(X_valid, self.weights), np.argmax(y_valid, 1))
+
+            print("training accuracy: " + str(round(training_accuracy, 2)))
+            print("validation accuracy: " + str(round(validation_accuracy, 2)))
+
+            print("cost: " + str(self._cost(X, y, self.weights)))
 
 
 def maxout(a):
-    return skimage.measure.block_reduce(a, (2,2), np.max)
+    return a[:13, :13]
+    # return skimage.measure.block_reduce(a, (2,2), np.max)
