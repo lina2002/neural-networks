7335773081873ed2ca99d65971e30da94e6eba37
Agnieszka Paszek <apaszek@mezzobit.com>
2018-10-31 04:01:44 +0100

    create script for running cifar10

---
+++ convolutional_cifar10.py
@@ -0,0 +1,33 @@
+import autograd.numpy as np
+import tensorflow as tf
+
+from convolutional_nn import ConvolutionalNN
+from extract_data import dense_to_one_hot
+from plotting import plot_confusion_matrices
+from utils import train_validation_split, compute_accuracy
+
+np.set_printoptions(suppress=True)
+np.set_printoptions(threshold=np.nan)
+
+
+if __name__ == "__main__":
+    (x_train, y_train), (x_test, y_test) = tf.keras.datasets.cifar10.load_data()
+    print(x_train.shape, y_train.shape, x_test.shape, y_test.shape)
+    y_train = dense_to_one_hot(y_train)
+    y_test = np.squeeze(y_test)
+
+    x_train, y_train, x_valid, y_valid = train_validation_split(x_train, y_train, training_set_size=45_000)
+
+    params = {'batch_size': 32,
+              'num_of_epochs': 10,
+              'learning_rate': 0.1,
+              'init_scale': 0.05,
+              'keep_prob': 0.9}
+    model = ConvolutionalNN(10, **params)
+    model.fit(x_train, y_train, x_valid, y_valid)
+
+    predictions = model.predict(x_test)
+
+    print("test accuracy: " + str(round(compute_accuracy(predictions, y_test), 2)))
+
+    plot_confusion_matrices(y_test, predictions, classes=range(10))
