903b48917677502874123870d999b590da2ff1a5
Agnieszka Paszek <apaszek@mezzobit.com>
2018-11-07 01:53:11 -0800

    multilayer tf working

--- multilayer_nn_tf.py
+++ multilayer_nn_tf.py
@@ -9,31 +9,30 @@ class MultiLayerNN:
     def __init__(self, sizes, batch_size, num_of_epochs, learning_rate, init_scale, keep_prob):
         self.batch_size = batch_size
         self.num_of_epochs = num_of_epochs
-        self.learning_rate = learning_rate
         self.keep_prob = keep_prob
 
         # this will populate weights using numbers from range [-init_scale, init_scale) with uniform distribution
         weights = []
-        # for s1, s2 in zip(sizes[:-1], sizes[1:]):
-        #     w = tf.Variable(tf.random_uniform([s1, s2], minval=-init_scale, maxval=init_scale))
-        #     weights.append(w)
-        w = tf.Variable(tf.random_uniform([784, 10], minval=-init_scale, maxval=init_scale))
-        weights.append(w)
+        for s1, s2 in zip(sizes[:-1], sizes[1:]):
+            w = tf.Variable(tf.random_uniform([s1, s2], minval=-init_scale, maxval=init_scale))
+            weights.append(w)
 
-        self.X = tf.placeholder(tf.float32, [None, 784])
-        self.y = tf.placeholder(tf.float32, [None, 10])
+        self.X = tf.placeholder(tf.float32, [None, sizes[0]])
+        self.y = tf.placeholder(tf.float32, [None, sizes[-1]])
+        self.prob = tf.placeholder_with_default(1.0, shape=())
         z = tf.matmul(self.X, weights[0])
-        # for w in weights[1:]:
-        #     z_r = tf.nn.relu(z)
-        #     z = tf.matmul(z_r, w)
+        for w in weights[1:]:
+            z_r = tf.nn.relu(z)
+            z_d = tf.nn.dropout(z_r, self.prob)
+            z = tf.matmul(z_d, w)
         self.y_pred = tf.nn.softmax(z)
-        cross_entropy = tf.nn.softmax_cross_entropy_with_logits(logits=z, labels=self.y)
-        self.optimizer = tf.train.AdamOptimizer(learning_rate).minimize(cross_entropy)
+        cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=z, labels=self.y))
+        self.optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(cross_entropy)
         self.sess = tf.InteractiveSession()
         self.sess.run(tf.global_variables_initializer())
 
-    def predict(self, xs):
-        return np.argmax(self.y_pred.eval({self.X: xs}), 1)
+    def predict(self, X):
+        return np.argmax(self.y_pred.eval({self.X: X}), 1)
 
     def fit(self, X_train, y_train, X_valid, y_valid):
         for epoch in range(self.num_of_epochs):
@@ -41,7 +40,8 @@ class MultiLayerNN:
             permuted_indices = np.random.permutation(X_train.shape[0])
             for i in range(0, X_train.shape[0], self.batch_size):
                 selected_data_points = np.take(permuted_indices, range(i, i+self.batch_size), mode='wrap')
-                self.sess.run(self.optimizer, {self.X: X_train[selected_data_points], self.y: y_train[selected_data_points]})
+                self.sess.run(self.optimizer, {self.X: X_train[selected_data_points],
+                                               self.y: y_train[selected_data_points], self.prob: self.keep_prob})
 
             training_accuracy = compute_accuracy(self.predict(X_train), np.argmax(y_train, 1))
             validation_accuracy = compute_accuracy(self.predict(X_valid), np.argmax(y_valid, 1))
