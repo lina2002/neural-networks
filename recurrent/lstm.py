b2ba40554188604dc383c72a708e4cccb4b6c396
Agnieszka Paszek <apaszek@mezzobit.com>
2018-12-01 00:02:24 +0100

    introduce batch size and embedding

--- recurrent/lstm.py
+++ recurrent/lstm.py
@@ -45,36 +45,58 @@ def indices_to_string(indices):
     return ''.join(index_to_char[i] for i in indices)
 
 
+def get_dropout_function(shape, keep_prob):
+    mask = get_dropout_mask(shape, keep_prob)
+
+    def dropout(x):
+        return np.multiply(x, mask)
+    return dropout
+
+
+def get_dropout_mask(shape, keep_prob):
+    rand = np.random.rand(*shape)
+    to_keep = rand < keep_prob
+    return to_keep
+
+
 class LSTM:
 
-    def __init__(self, x_size, y_size, num_of_epochs, learning_rate, init_scale, number_of_steps):
+    def __init__(self, x_size, y_size, num_of_epochs, learning_rate, init_scale, number_of_steps, batch_size, keep_prob):
         self.num_of_epochs = num_of_epochs
         self.h_size = h_size = 200
-        self.batch_size = 20
+        self.x_size = x_size
+        self.y_size = y_size
+        self.batch_size = batch_size
         self.number_of_steps = number_of_steps
         self.learning_rate = learning_rate
         self.init_scale = init_scale
+        self.keep_prob = keep_prob
         self.C = np.zeros(h_size)
         self.h = np.zeros(h_size)
-        self.W_f = self._random_matrix((h_size, h_size + x_size))
+        self.W_1 = self._random_matrix((h_size, x_size))
+        self.b_1 = self._random_matrix((h_size,))
+        self.W_f = self._random_matrix((h_size, h_size + h_size))
         self.b_f = self._random_matrix((h_size,)) + 1
-        self.W_i = self._random_matrix((h_size, h_size + x_size))
+        self.W_i = self._random_matrix((h_size, h_size + h_size))
         self.b_i = self._random_matrix((h_size,))
-        self.W_c = self._random_matrix((h_size, h_size + x_size))
+        self.W_c = self._random_matrix((h_size, h_size + h_size))
         self.b_c = self._random_matrix((h_size,))
-        self.W_o = self._random_matrix((h_size, h_size + x_size))
+        self.W_o = self._random_matrix((h_size, h_size + h_size))
         self.b_o = self._random_matrix((h_size,))
-        self.W = self._random_matrix((y_size, h_size))
-        self.b = self._random_matrix((y_size,))
-        self.weights = [self.W_f, self.b_f, self.W_i, self.b_i, self.W_c, self.b_c, self.W_o, self.b_o, self.W, self.b]
+        self.W_2 = self._random_matrix((y_size, h_size))
+        self.b_2 = self._random_matrix((y_size,))
+        self.weights = [self.W_1, self. b_1, self.W_f, self.b_f, self.W_i, self.b_i, self.W_c, self.b_c, self.W_o, self.b_o, self.W_2, self.b_2]
 
     def _cost(self, inputs, targets, hprev, Cprev, weights, disable_tqdm=True):
-        W_f, b_f, W_i, b_i, W_c, b_c, W_o, b_o, W, b = weights
+        W_1, b_1, W_f, b_f, W_i, b_i, W_c, b_c, W_o, b_o, W_2, b_2 = weights
         h = np.copy(hprev)
         C = np.copy(Cprev)
+
         loss = 0
         for t in tqdm(range(len(inputs)), disable=disable_tqdm):
             x = char_to_one_hot(inputs[t])
+
+            x = np.matmul(W_1, x) + b_1
 
             f = sigmoid(np.matmul(W_f, np.concatenate((h, x))) + b_f)
             i = sigmoid(np.matmul(W_i, np.concatenate((h, x))) + b_i)
@@ -82,7 +104,7 @@ class LSTM:
             C = f*C + i*C_hat
             o = sigmoid(np.matmul(W_o, np.concatenate((h, x))) + b_o)
             h = o*np.tanh(C)
-            y = np.matmul(W, h) + b
+            y = np.matmul(W_2, h) + b_2
 
             target_index = char_to_index[targets[t]]
             # ps_target[t] = np.exp(ys[t][target_index])/np.sum(np.exp(ys[t]))  # probability for next chars being target
@@ -93,23 +115,39 @@ class LSTM:
         return loss
 
     def _cost_batched(self, inputs, targets, hprev, Cprev, weights, disable_tqdm=True):
-        W_f, b_f, W_i, b_i, W_c, b_c, W_o, b_o, W, b = weights
+        W_1, b_1, W_f, b_f, W_i, b_i, W_c, b_c, W_o, b_o, W_2, b_2 = weights
         h = np.copy(hprev)
         C = np.copy(Cprev)
         h = h.reshape((self.batch_size, self.h_size, 1))
         C = C.reshape((self.batch_size, self.h_size, 1))
         loss = 0
+        # W_sth_dropout = get_dropout_function((self.h_size, self.h_size + self.x_size), self.keep_prob)
+        # b_sth_dropout = get_dropout_function((self.h_size,), self.keep_prob)
+        # W_dropout = get_dropout_function((self.y_size, self.h_size), self.keep_prob)
+        # b_dropout = get_dropout_function((self.y_size,), self.keep_prob)
+        cell_dropout = get_dropout_function((self.batch_size, self.h_size, 1), self.keep_prob)
+        y_dropout = get_dropout_function((self.batch_size, self.y_size, 1), self.keep_prob)
         for t in tqdm(range(len(inputs)), disable=disable_tqdm):
             x = np.array([char_to_one_hot(c) for c in inputs[:, t]])
             x = x.reshape((self.batch_size, -1, 1))
 
+            x = np.matmul(W_1, x) + np.reshape(b_1, (-1, 1))
+            x = cell_dropout(x)
+
             f = sigmoid(np.matmul(W_f, np.concatenate((h, x), axis=1)) + np.reshape(b_f, (-1, 1)))
+            f = cell_dropout(f)
             i = sigmoid(np.matmul(W_i, np.concatenate((h, x), axis=1)) + np.reshape(b_i, (-1, 1)))
+            i = cell_dropout(i)
             C_hat = np.tanh(np.matmul(W_c, np.concatenate((h, x), axis=1)) + np.reshape(b_c, (-1, 1)))
+            C_hat = cell_dropout(C_hat)
             C = f*C + i*C_hat
+            C = cell_dropout(C)
             o = sigmoid(np.matmul(W_o, np.concatenate((h, x), axis=1)) + np.reshape(b_o, (-1, 1)))
+            o = cell_dropout(o)
             h = o*np.tanh(C)
-            ys = np.matmul(W, h) + np.reshape(b, (-1, 1))
+            h = cell_dropout(h)
+            ys = np.matmul(W_2, h) + np.reshape(b_2, (-1, 1))
+            ys = y_dropout(ys)
 
             target_indices = np.array([char_to_index[c] for c in targets[:, t]])
             # ps_target[t] = np.exp(ys[t][target_index])/np.sum(np.exp(ys[t]))  # probability for next chars being target
@@ -130,9 +168,11 @@ class LSTM:
         return np.exp(self._cost(data[:-1], data[1:], self.h, self.C, self.weights, disable_tqdm=False))
 
     def _update_hidden_state(self, inputs, weights):
-        W_f, b_f, W_i, b_i, W_c, b_c, W_o, b_o, W, b = weights
+        W_1, b_1, W_f, b_f, W_i, b_i, W_c, b_c, W_o, b_o, W_2, b_2 = weights
         for t in range(len(inputs)):
             x = char_to_one_hot(inputs[t])
+
+            x = np.matmul(W_1, x) + b_1
 
             f = sigmoid(np.matmul(W_f, np.concatenate((self.h, x))) + b_f)
             i = sigmoid(np.matmul(W_i, np.concatenate((self.h, x))) + b_i)
@@ -143,12 +183,14 @@ class LSTM:
         return self.h
 
     def _update_hidden_state_batched(self, inputs, h, C, weights):
-        W_f, b_f, W_i, b_i, W_c, b_c, W_o, b_o, W, b = weights
+        W_1, b_1, W_f, b_f, W_i, b_i, W_c, b_c, W_o, b_o, W_2, b_2 = weights
         h = h.reshape((self.batch_size, self.h_size, 1))
         C = C.reshape((self.batch_size, self.h_size, 1))
         for t in range(len(inputs)):
             x = np.array([char_to_one_hot(c) for c in inputs[:, t]])
             x = x.reshape((self.batch_size, -1, 1))
+
+            x = np.matmul(W_1, x) + np.reshape(b_1, (-1, 1))
 
             f = sigmoid(np.matmul(W_f, np.concatenate((h, x), axis=1)) + np.reshape(b_f, (-1, 1)))
             i = sigmoid(np.matmul(W_i, np.concatenate((h, x), axis=1)) + np.reshape(b_i, (-1, 1)))
@@ -164,13 +206,14 @@ class LSTM:
         x = char_to_one_hot(seed)
         ixes = []
         for t in range(number_of_characters_to_generate):
+            x = np.matmul(self.W_1, x) + self.b_1
             f = sigmoid(np.matmul(self.W_f, np.concatenate((h, x))) + self.b_f)
             i = sigmoid(np.matmul(self.W_i, np.concatenate((h, x))) + self.b_i)
             C_hat = np.tanh(np.matmul(self.W_c, np.concatenate((h, x))) + self.b_c)
             C = f*C + i*C_hat
             o = sigmoid(np.matmul(self.W_o, np.concatenate((h, x))) + self.b_o)
             h = o*np.tanh(C)
-            y = np.matmul(self.W, h) + self.b
+            y = np.matmul(self.W_2, h) + self.b_2
 
             p = np.exp(y)/np.sum(np.exp(y))
             ix = np.random.choice(range(alphabet_size), p=p)
@@ -230,9 +273,11 @@ class LSTM:
         print(self.perplexity(test_data))
 
 
-params = {'num_of_epochs': 20,
-          'learning_rate': 8,
+params = {'num_of_epochs': 35,
+          'learning_rate': 4,
           'init_scale': 0.1,
-          'number_of_steps': 25}
+          'number_of_steps': 20,
+          'batch_size': 20,
+          'keep_prob': 0.9}
 lstm = LSTM(alphabet_size, alphabet_size, **params)
 lstm.fit()
